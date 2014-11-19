0044eef5148a12d3b2615936842f2492c5e31ef1
Amit <amtaylor@gmail.com>
2014-11-18 21:28:17 -0800

    added morgan

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
- 
-app.use(express.logger('dev'));
+var logger = require('morgan');
+
+app.use(logger); //replaces your app.use(express.logger()); 
+//app.use(express.logger('dev')); 
 app.use(gzippo.staticGzip("" + __dirname + "/app"));
 app.listen(process.env.PORT || 5000);
